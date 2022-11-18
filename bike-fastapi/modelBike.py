1052d3d836bf5c4ec6d1ff7562bcc930b0b3f0fc
jouad01 <jouardiouardi@cifpfbmoll.eu>
2022-11-18 21:03:24 +0100

    fix: fix the updateBike

--- bike-fastapi/modelBike.py
+++ bike-fastapi/modelBike.py
@@ -27,17 +27,26 @@ class Bicycle(BaseModel):
             }
         }
 
+# Make a update model for the bike
 class BicycleUpdate(BaseModel):
+    # This is the model for the Bicycle collection in the BaleaBike database with hes attributes
+    booking : Optional[bool] = False
+    price : Optional[float] = 0.0
     material : Optional[str] = None
     components : Optional[bool] = False
     typeBike : Optional[str] = None
+    year : Optional[int] = 0
+
 
     class Config:
+        allow_population_by_field_name = True
         schema_extra = {
             "example": {
+                "booking": False,
+                "price": 200,
                 "material": "Carbono",
                 "components": True,
                 "typeBike": "city",
+                "year": 2019
             }
         }
-    
